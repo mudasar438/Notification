a7cdfa2ea0a4b38284f4e67ea038675b36bbf44d
essarooman <essarooman623@gmail.com>
2024-11-10 21:45:00 +0500

    ch

--- server/Routs/notification.js
+++ server/Routs/notification.js
@@ -8,7 +8,14 @@ router.get("/:userId", async (req, res) => {
   const notifications = await Notification.find({ userId: userId });
   res.json(notifications);
 });
-
+router.post("/addNotification", async (req, res) => {
+  const notifications = new Notification({
+    userId: req.body.userId,
+    message: `this is a Notification`,
+  });
+  const saved = await notifications.save();
+  res.json(saved);
+});
 router.put("/:id", async (req, res) => {
   const notification = await Notification.findByIdAndUpdate(req.params.id, {
     isRead: true,
